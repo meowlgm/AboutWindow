91b3e3a5c5858bdef92648873ec4e249c0f6e578
Giorgi T <bulbs.carrots_0a@icloud.com>
2025-06-02 02:19:12 +0400

    - Moved AboutWindow files from CodeEdit into this package.

---
+++ Sources/AboutWindow/Views/AboutView.swift
@@ -0,0 +1,74 @@
+//
+//  AboutView.swift
+//  CodeEditModules/About
+//
+//  Created by Andrei Vidrasco on 02.04.2022
+//
+
+import SwiftUI
+
+enum AboutMode: String, CaseIterable {
+    case about
+    case acknowledgements
+    case contributors
+}
+
+public struct AboutView: View {
+    @Environment(\.openURL)
+    private var openURL
+    @Environment(\.colorScheme)
+    private var colorScheme
+    @Environment(\.dismiss)
+    private var dismiss
+
+    @State var aboutMode: AboutMode = .about
+
+    @Namespace var animator
+
+    public var body: some View {
+        ZStack(alignment: .top) {
+            switch aboutMode {
+            case .about:
+                AboutDefaultView(aboutMode: $aboutMode, namespace: animator)
+            case .acknowledgements:
+                AcknowledgementsView(aboutMode: $aboutMode, namespace: animator)
+            case .contributors:
+                ContributorsView(aboutMode: $aboutMode, namespace: animator)
+            }
+        }
+        .animation(.smooth, value: aboutMode)
+        .ignoresSafeArea()
+        .frame(width: 280, height: 400 - 28)
+        .fixedSize()
+        // hack required to get buttons appearing correctly in light appearance
+        // if anyone knows of a better way to do this feel free to refactor
+//        .background(.regularMaterial.opacity(0))
+        .background {
+            if colorScheme == .dark {
+                Color(.black).opacity(0.075)
+                    .background(.thickMaterial)
+            } else {
+                Color(.white).opacity(0.6)
+                    .background(.regularMaterial)
+            }
+        }
+        .background {
+            Button("") {
+                dismiss()
+            }
+            .keyboardShortcut(.escape, modifiers: [])
+            .hidden()
+        }
+        .task {
+            if let window = NSApp.findWindow(DefaultSceneID.about) {
+                window.styleMask = [.closable, .fullSizeContentView, .titled, .nonactivatingPanel]
+                window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                window.standardWindowButton(.zoomButton)?.isHidden = true
+                window.titleVisibility = .hidden
+                window.titlebarAppearsTransparent = true
+                window.backgroundColor = .clear
+                window.isMovableByWindowBackground = true
+            }
+        }
+    }
+}
