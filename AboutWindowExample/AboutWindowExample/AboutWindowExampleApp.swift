bc090c9dd52ae54ce6fcacc5b37e0b5e76a8ce55
Giorgi T <bulbs.carrots_0a@icloud.com>
2025-06-02 16:29:38 +0400

    - Updating internals

---
+++ AboutWindowExample/AboutWindowExample/AboutWindowExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutWindowExampleApp.swift
+//  AboutWindowExample
+//
+//  Created by Giorgi Tchelidze on 02.06.25.
+//
+
+import SwiftUI
+
+@main
+struct AboutWindowExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
