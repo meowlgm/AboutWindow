1127ee995a09c0fea365ab5d796d7f2c3a4cf59b
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-07-07 13:57:11 -0500

    Fix Docc Path

--- .github/scripts/build-docc.sh
+++ .github/scripts/build-docc.sh
@@ -5,4 +5,4 @@ export LC_CTYPE=en_US.UTF-8
 set -o pipefail && xcodebuild clean docbuild -scheme AboutWindow \
     -destination generic/platform=macos \
     -skipPackagePluginValidation \
-    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path WelcomeWindow --output-path ./docs" | xcpretty
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path AboutWindow --output-path ./docs" | xcpretty
